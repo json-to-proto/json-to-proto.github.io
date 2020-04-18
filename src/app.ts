7c0aeeb31f82d5057fa6babfb75d204b83a6cbc1
YaroslavPodorvanov <yaroslav.podorvanov@gmail.com>
2020-04-19 00:55:37 +0300

    empty object convert

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,15 @@ const $input = document.getElementById("input");
 const $output = document.getElementById("output");
 
 $input.addEventListener("keyup", function () {
-    $output.innerHTML = convert($input.innerHTML);
+    $output.innerHTML = convert($input.innerHTML.trim());
 })
 
 function convert(source: string): string {
+    if (source === "{}") {
+        return `syntax = "proto3";
+
+message SomeMessage {}`;
+    }
+
     return source;
 }
