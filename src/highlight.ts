2db1ab3f2e6ee8e2c0d277cb92dd2e060cf23493
YaroslavPodorvanov <yaroslav.podorvanov@gmail.com>
2020-04-19 14:18:18 +0300

    highlight.js dynamically

--- src/highlight.ts
+++ src/highlight.ts
@@ -4,5 +4,23 @@ import protobuf from "@highlightjs/highlight.js/lib/languages/protobuf";
 hljs.registerLanguage("protobuf", protobuf);
 
 export default function highlight(source: string): string {
+    initOnce();
+
     return hljs.highlight("protobuf", source).value;
+}
+
+let initOnce = function() {
+    css("https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.18.1/styles/default.min.css");
+
+    initOnce = function () {};
+};
+
+function css(url) {
+    const head = document.getElementsByTagName('head')[0];
+    const link = document.createElement('link');
+
+    link.rel = "stylesheet";
+    link.href = url;
+
+    head.appendChild(link);
 }
