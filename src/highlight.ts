7280a52aa7c346f759eb62ed40b7ad62ccb287f2
YaroslavPodorvanov <yaroslav.podorvanov@gmail.com>
2022-08-17 03:27:35 +0300

    remove lazy

--- src/highlight.ts
+++ src/highlight.ts
@@ -4,23 +4,5 @@ import protobuf from "highlight.js/lib/languages/protobuf";
 hljs.registerLanguage("protobuf", protobuf);
 
 export default function highlight(source: string): string {
-    initOnce();
-
     return hljs.highlight(source, {language: "protobuf"}).value;
-}
-
-let initOnce = function() {
-    css("https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/styles/default.min.css");
-
-    initOnce = function () {};
-};
-
-function css(url) {
-    const head = document.getElementsByTagName('head')[0];
-    const link = document.createElement('link');
-
-    link.rel = "stylesheet";
-    link.href = url;
-
-    head.appendChild(link);
 }
